cd3b7aec51eea09f4570174f782f8b500dcdb9e1
PedroHenriqueSales <pedrohsalesf@gmail.com>
2016-10-09 20:23:01 -0300

    Applying default behavior in Geometry class

--- src/Geometry.java
+++ src/Geometry.java
@@ -30,6 +30,8 @@ public class Geometry {
 					Polygon3D temp = polygons[j+1];
 					polygons[j+1] = polygons[j];
 					polygons[j] = temp;
+				}else{
+					// Do nothing
 				}
 			}
 		}
@@ -43,47 +45,52 @@ public class Geometry {
 		
 		if(!a.visible){
 			return false;
-		}
-		if(!b.visible){
+		}else if(!b.visible){
 			return true;
-		}
-	
-		if(a.tempVertex[0].z < b.tempVertex[0].z && a.tempVertex[1].z < b.tempVertex[1].z && a.tempVertex[2].z < b.tempVertex[2].z && a.tempVertex[3].z < b.tempVertex[3].z){
+		}else if(a.tempVertex[0].z < b.tempVertex[0].z && a.tempVertex[1].z < b.tempVertex[1].z && a.tempVertex[2].z < b.tempVertex[2].z && a.tempVertex[3].z < b.tempVertex[3].z){
 			return true;	
-		}	
-
+		}else{
 		
-		for(int i = 0; i < b.tempVertex.length; i++) {
-			tempVector.set(b.tempVertex[i]);
-			tempVector.subtract(a.centre);
-			tempVector.unit();
-
-			if(tempVector.dot(a.normal) > 0.0001 ) {
-				inside = false;
-				break;
+			for(int i = 0; i < b.tempVertex.length; i++) {
+				tempVector.set(b.tempVertex[i]);
+				tempVector.subtract(a.centre);
+				tempVector.unit();
+	
+				if(tempVector.dot(a.normal) > 0.0001 ) {
+					inside = false;
+					break;
+				}else{
+					// Do nothing
+				}
+	
 			}
-
-		}
-		if(inside){
-			return true;
-		}
-
-		inside = true;
-		
-		for(int i = 0; i < a.tempVertex.length; i++) {
-			tempVector.set(a.tempVertex[i]);
-			tempVector.subtract(b.centre);
-			tempVector.unit();
-
-			if(tempVector.dot(b.normal) < -0.0001 ) {
-				inside = false;
-				break;
+			if(inside){
+				return true;
+			} else{
+				// Do nothind
 			}
+	
+			inside = true;
+			
+			for(int i = 0; i < a.tempVertex.length; i++) {
+				tempVector.set(a.tempVertex[i]);
+				tempVector.subtract(b.centre);
+				tempVector.unit();
+	
+				if(tempVector.dot(b.normal) < -0.0001 ) {
+					inside = false;
+					break;
+				}else {
+					// Do nothing
+				}
+			}
+	
+			if(inside){
+				return true;
+			}else{
+				// Do nothing
+			}
+			return false;
 		}
-
-		if(inside){
-			return true;
-		}
-		return false;
 	}
 }
