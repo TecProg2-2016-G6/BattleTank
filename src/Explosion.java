1980c37a559218f8acba8a53db9c59a07ae40d1c
PedroHenriqueSales <pedrohsalesf@gmail.com>
2016-10-10 10:33:29 -0300

    Correct structure and clarity of overwrite methods

--- src/Explosion.java
+++ src/Explosion.java
@@ -27,30 +27,30 @@ public class Explosion extends SolidObject {
 		
 		int random = GameData.getRandom();
 		
-		startPointInWorld = new Vector(xAxis,yAxys,zAxys);
-		iDirection = new Vector(1,0,0);
-		jDirection = new Vector(0,1,0);
-		kDirection = new Vector(0,0,1);
-		lifeSpanObject = 16;
+		this.startPointInWorld = new Vector(xAxis,yAxys,zAxys);
+		this.iDirection = new Vector(1,0,0);
+		this.jDirection = new Vector(0,1,0);
+		this.kDirection = new Vector(0,0,1);
+		this.lifeSpanObject = 16;
 		
 		//Create 2D boundary
-		boundaryModel2D = new Rectangle2D(xAxis - 0.1, zAxys + 0.1, 0.2, 0.2);
+		this.boundaryModel2D = new Rectangle2D(xAxis - 0.1, zAxys + 0.1, 0.2, 0.2);
 		
 		//Find centre of explosion
-		centreOfExplosionInTileMap = (int)(xAxis*4) + (129-(int)(zAxys*4))*80;
+		this.centreOfExplosionInTileMap = (int)(xAxis*4) + (129-(int)(zAxys*4))*80;
 		 
 		if(random >= 75 ) {			
-			spriteExplosionIndex = 17;
+			this.spriteExplosionIndex = 17;
 		}else if(random >= 50 ) {			
-			spriteExplosionIndex = 18;
+			this.spriteExplosionIndex = 18;
 		}else if(random >= 25) {
-			spriteExplosionIndex = 19;
+			this.spriteExplosionIndex = 19;
 		}else{
-			spriteExplosionIndex = 20;
+			this.spriteExplosionIndex = 20;
 		}
 		
 		if(size > 1){
-			spriteExplosionIndex = 18;
+			this.spriteExplosionIndex = 18;
 		}
 		
 		Vector[] v = new Vector[]{put(-0.3, 0, 0.3), put(0.3, 0, 0.3), put(0.3, 0, -0.3),put(-0.3, 0, -0.3)};
@@ -61,7 +61,7 @@ public class Explosion extends SolidObject {
 			// Do nothing
 		}
 		
-		explosionAura = new Polygon3D(v, v[0], v[1], v[3], Main.textures[21], 1, 1, 2);
+		this.explosionAura = new Polygon3D(v, v[0], v[1], v[3], Main.textures[21], 1, 1, 2);
 		
 		this.sizeOfExplosion = size;
 		
@@ -75,64 +75,67 @@ public class Explosion extends SolidObject {
 	}
 	
 	//Animate explosion scene
+	@Override
 	public void update() {
 		//Make always visible
-		isVisible = true;
+		this.isVisible = true;
 		
-		if(explosionAura != null && damage != 0) {
-			explosionAura.update();
+		if(this.explosionAura != null && this.damage != 0) {
+			this.explosionAura.update();
 			
-			if(explosionAura.visible) {
-				explosionAura.myTexture.Texture = explosionAura.myTexture.lightMapData[currentAuraIndex];
-				Rasterizer.rasterize(explosionAura);
+			if(this.explosionAura.visible) {
+				this.explosionAura.myTexture.Texture = this.explosionAura.myTexture.lightMapData[this.currentAuraIndex];
+				Rasterizer.rasterize(this.explosionAura);
 			}
 		}
-		currentAuraIndex++;
+		this.currentAuraIndex++;
 		
 		//Send to draw list
-		if(!explicitDrawing)
+		if(!this.explicitDrawing)
 			ModelDrawList.register(this);
 		
 		//Find centre in camera coordinate
 		
-		cantreModelInCamera.set(centreModel);
-		cantreModelInCamera.subtract(Camera.cameraPosition);
-		cantreModelInCamera.rotate_XZ(Camera.XZ_angle);
-		cantreModelInCamera.rotate_YZ(Camera.YZ_angle);
+		this.cantreModelInCamera.set(this.centreModel);
+		this.cantreModelInCamera.subtract(Camera.cameraPosition);
+		this.cantreModelInCamera.rotate_XZ(Camera.XZ_angle);
+		this.cantreModelInCamera.rotate_YZ(Camera.YZ_angle);
 		
 		
 		//Damage nearby units
-		if(lifeSpanObject == 15 && damage != 0) {
-			ObstacleMap.damageType2Obstacles(damage, boundaryModel2D, centreOfExplosionInTileMap);
+		if(this.lifeSpanObject == 15 && this.damage != 0) {
+			ObstacleMap.damageType2Obstacles(this.damage, this.boundaryModel2D, this.centreOfExplosionInTileMap);
 		}
 		
-		lifeSpanObject--;
+		this.lifeSpanObject--;
 		
-		if(lifeSpanObject == 0){
-			lifeSpanObject = -1;
+		if(this.lifeSpanObject == 0){
+			this.lifeSpanObject = -1;
 			return;
 		}
 	}
 	
 	//Draw  explosion scene
+	@Override
 	public void drawExplosion() {
 		
 		//Calculate explosion size
-		cantreModelInCamera.updateLocation();
-		double ratio = sizeOfExplosion*2/cantreModelInCamera.z;
+		this.cantreModelInCamera.updateLocation();
+		double ratio = this.sizeOfExplosion*2/this.cantreModelInCamera.z;
 		
 		
 		//Drawing sprite	
 		Rasterizer.temp = this.typeOfExplosion; 
-		Rasterizer.renderExplosionSprite(Main.textures[spriteExplosionIndex].explosions[currentFrameIndex],ratio, cantreModelInCamera.screenX, cantreModelInCamera.screenY, 64, 64);
+		Rasterizer.renderExplosionSprite(Main.textures[this.spriteExplosionIndex].explosions[this.currentFrameIndex],ratio, this.cantreModelInCamera.screenX, this.cantreModelInCamera.screenY, 64, 64);
 		
-		currentFrameIndex++;
+		this.currentFrameIndex++;
 		
 	}
 	
 	//Return the 2D boundary of this model
+	@Override
 	public Rectangle2D getBoundary2D() {
-		return boundaryModel2D;
+		return this.boundaryModel2D;
 	}
 	
 	
